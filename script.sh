bccce74c28cd2655b25635d52e7ba40f5dcb81a1
fluctlights <antoniofernando.mateo@alu.uclm.es>
2022-05-28 04:26:21 +0200

    Ya es funcional

--- script.sh
+++ script.sh
@@ -1,54 +1,58 @@
 #!/bin/bash
 
+# -- Variables globales -- #
+
+USUARIO="" #email
+PASS=""
+HOST=""
+IP=""
+WEB="@dynupdate.no-ip.com/nic/update?"
+
+# -- Metodos -- #
+
+function recoger_args
+{
+    if [ $@ -ne 4 ]; then     # si no tenemos los argumentos necesarios salimos
+        echo "ERROR! Uso: ./script.sh '<usuario>' '<password>' '<host>'"
+        exit -1
+    fi
+
+    USUARIO="${USUARIO}$1" 
+    PASS="${PASS}$2"
+    HOST="${HOST}$3"
+}
+
 function obtener_ip
 {
-    IP = $(wget -qO- "http://myexternalip.com/raw") #obtencion de IP publica
+    IP=$(wget -qO- "http://myexternalip.com/raw") #obtencion de IP publica
 
-    if [ -z $IP ] #error al obtener IP
+    if [ $IP -eqz ]; #error al obtener IP
     then
         echo "NO se ha podido obtener la IP! Saliendo ..."
         exit -1
     fi
 }
 
-function recoger_args
-{
+function realizar_peticion { python3 -c "
     
-    if [$# -ne 3]; then     # si no tenemos los argumentos necesarios salimos
-        echo "ERROR! Uso: ./script.sh <usuario> <password> <host>"
-        exit -1
-
-    USUARIO = "${USUARIO}$1" 
-    PASS = "${PASS}$2"
-    HOST = "${HOST}$3"
+import requests
+import sys
+url = 'http://${AUTH}${WEB}hostname=${HOST}&ip=${IP}'
+print(url)
+r = requests.get(url)
+if r.status_code != 200:
+    print('Error al realizar la peticion! Saliendo...')
+    sys.exit(1)
+else:
+    print('Hostname actualizado correctamente')
+";
 }
 
-function realizar_peticion
-{
-    python3 -c "
-    
-    import requests
-    url = 'http://' + ${AUTH} + ${WEB} + 'hostname=' + ${HOST} + '&ip=' + ${IP}
-    r = requests.get(url)
-    if r..status_code != 200:
-        print('Error al realizar la peticion! Saliendo...')
-        sys.exit(1)
-    ";
-}
-
-USUARIO = "" #email
-PASS = ""
-HOST = ""
-IP = ""
-WEB = "@dynupdate.no-ip.com/nic/update?"
+# -- ToDo -- #
 
 recoger_args
 obtener_ip
 realizar_peticion
-
-
-
-
 
 
 
