20a57561250a58f246b608e51e7535e7a3c50159
Antonio Mateo <91947321+fluctlights@users.noreply.github.com>
2022-07-08 11:06:45 +0200

    Actualizado - falta añadir el email del usuario

--- script.sh
+++ script.sh
@@ -1,83 +1,50 @@
 #!/bin/bash
 
-# ------------------------- Variables globales ----------------------- #
+# ------------------ Variables globales -------------------- #
 
-USUARIO="" # email!!
+EMAIL=""
 PASS=""
-HOST="" # nombre del dominio que queremos actualizar
-IP="" # direccion IP la cual el dominio nos referenciara
-URL_REQUEST="" # url sobre la que realizaremos la peticion
+DOMINIO=""
+IP=""
+WEB="dynupdate.no-ip.com/nic/update"
 
-WEB="https://dynupdate.no-ip.com/nic/update?"
-USERAGENT="--user-agent 'no-ip shell script/1.0 mail@mail.com'" #atributo para cURL
+# ---------------------- Funciones ------------------------- #
 
-# ---------------------------- Metodos ------------------------------ #
-
-function obtener_credenciales
+function recoger_args
 {
-    
-    # Nota : para hacerlo automatico (uso con cron) borrar el condicional 
-    #         y poner los valores concretos directamente justo debajo
-    
     if [ $@ -ne 4 ]; then     # si no tenemos los argumentos necesarios salimos
-        echo "ERROR!! Uso: ./script.sh '<usuario>' '<password>' '<host_para_actualizar>'"
+        echo "ERROR! Uso: ./script.sh '<email>' '<contrasena>' '<dominio>'"
         exit -1
     fi
 
-    USUARIO="$1" 
-    PASS="$2"
-
-    # Ciframos las credenciales
-    BASE64_PASS=$(echo "$USUARIO:$PASS" | base64)
-    AUTH_HEADERS="\"Authorization: Basic ${BASE64_PASS}"\" #parametro para cURL
-
+    EMAIL="${EMAIL}$1" 
+    PASS="${PASS}$2"
+    DOMINIO="${DOMINIO}$3"
 }
 
-function obtener_datos_host-ip
+function obtener_ip
 {
-    HOST="hostname=$3"
-    
-    IP=$(wget -qO- "http://myexternalip.com/raw") # obtencion de IP publica
+    IP=$(curl ifconfig.me) #obtencion de IP publica
+    IP_2=$(curl https://ipinfo.io/ip) # variante
 
-    if [ $IP -eqz ]; # error al obtener IP
+    if [ -z "$IP" ]; # campo IP vacio
     then
-        echo "NO se ha podido obtener la IP! Saliendo ..."
+        echo "ERROR! No se ha podido obtener la IP! Saliendo ..."
         exit -1
     fi
-
-    # Actualizamos los datos de la URL
-    URL_REQUEST="${WEB}${HOST}&ip=${IP}"
 }
 
-function realizar_peticion { python3 -c "
-    
-import requests
-import sys
-url = 'http://${AUTH}${WEB}hostname=${HOST}&ip=${IP}'
-r = requests.get(url)
-if r.status_code != 200:
-    print('Error al realizar la peticion! Saliendo...')
-    sys.exit(1)
-else:
-    print('Hostname actualizado correctamente')
-";
+function realizar_peticion
+{
+    AGENTE="bash-curl-cron/1.0 youremail@yourdomain.com" # necesario, sin el corremos riesgo de que la peticion se bloquee
+    COMANDO="curl --user-agent $AGENTE --silent https://$USUARIO:$PASS@$WEB?hostname=${HOST}&myip=${IP}"
+    echo "Realizando peticion"
+    RESULTADO=$(curl $COMANDO)
+    echo $RESULTADO
 }
 
-function realizar_peticion_bash { 
-
-    CMD=$(curl -X GET ${URL_REQUEST} -H ${AUTH_HEADERS})
-
-    # Comprobacion de errores
-    RESULT=$(eval ${CMD})
-
-	if [ -z "$RESULT" ] && [ $? -ne 0 ]
-        echo "Problema al actualizar el host"
-    fi
-}
-
-# -------------------------------- ToDo -------------------------------- #
+# ----------------------- ToDo ----------------------- #
 
 recoger_args
 obtener_ip
 realizar_peticion
-# realizar_peticion_bash
